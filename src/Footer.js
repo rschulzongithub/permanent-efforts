02dc924cb049e4b7b7186e49d1791fc4a2e61b9e
Robert Schulz <rschulz.hamburg@gmail.com>
2019-11-29 16:31:34 +0100

    Icons in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,38 @@
+import React from 'react'
 import styled from 'styled-components/macro'
+import TimeIcon from './timer-icon.svg'
+import ConstructorIcon from './construction-icon.svg'
 
-export default styled.div`
-  background: linear-gradient(45deg, #92a5a1 0%, #596c68 100%);
-`
+export default function Footer() {
+  const Navigation = styled.nav`
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: repeat(4, auto);
+    background: linear-gradient(45deg, #92a5a1 0%, #596c68 100%);
+    border: 2px solid black;
+    border-radius: 10px;
+  `
+  const NavIcon = styled.button`
+    background: transparent;
+  `
+  const IconImg = styled.img`
+    max-height: 100%;
+    max-width: 100%;
+  `
+  return (
+    <Navigation>
+      <NavIcon>
+        <IconImg src={TimeIcon} alt="" height="30" width="30" />
+      </NavIcon>
+      <NavIcon>
+        <IconImg src={ConstructorIcon} alt="" height="25" width="25" />
+      </NavIcon>
+      <NavIcon>
+        <IconImg src={TimeIcon} alt="" height="30" width="30" />
+      </NavIcon>
+      <NavIcon>
+        <IconImg src={TimeIcon} alt="" height="30" width="30" />
+      </NavIcon>
+    </Navigation>
+  )
+}
